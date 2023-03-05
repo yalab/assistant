7a6ac32082bbe589d65f7d5af70c01c8997816d5
yalab <rudeboyjet@gmail.com>
2023-03-05 18:59:10 +0900

    use dotenvy.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+    dotenv().ok();
+    let api_key = env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY environment variable is not found.");
+    println!("{}", api_key);
 }
