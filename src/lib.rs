e0b8d3d7891dcec9d282bd6378599fba3056c6aa
yalab <rudeboyjet@gmail.com>
2023-03-27 18:43:46 +0900

    Achieved the ability to perform an HTTP GET request for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 use dotenvy::dotenv;
 use std::env;
+use reqwest::blocking::Client;
 
 pub fn api_key() -> String {
     dotenv().ok();
     let api_key = env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY environment variable is not found.");
     return api_key;
 }
+
+pub fn call_chat_api(api_key: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let resp = client.get("https://google.com").send()?;
+    println!("{}", api_key);
+    println!("{:#?}", resp.text()?);
+    Ok(())
+}
+
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -2,5 +2,12 @@ use assistant::*;
 
 #[test]
 fn api_key_test () {
-    assert_eq!(52, api_key().len());
+    assert_eq!(51, api_key().len());
+}
+
+#[test]
+fn call_chat_api_test() {
+    let api_key = api_key();
+    let result = call_chat_api(&api_key);
+    assert!(result.is_ok());
 }
