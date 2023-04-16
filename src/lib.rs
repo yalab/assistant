5f121a20da85066761b2fe9b4a1510edcf27fc5c
yalab <rudeboyjet@gmail.com>
2023-04-16 15:59:32 +0900

    Mute warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 use dotenvy::dotenv;
-use std::env;
 use reqwest::blocking::{Client, Response};
 use reqwest::header::{HeaderMap, HeaderValue};
+use std::env;
 
 pub fn api_key() -> String {
     dotenv().ok();
-    let api_key = env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY environment variable is not found.");
-    return api_key;
+    env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY environment variable is not found.")
 }
 
 pub fn call_chat_api(api_key: &str) -> Result<Response, Box<dyn std::error::Error>> {
@@ -16,9 +15,8 @@ pub fn call_chat_api(api_key: &str) -> Result<Response, Box<dyn std::error::Erro
 
     let client = Client::new();
     let response = client
-	.get("https://api.openai.com/v1/models")
-	.headers(headers)
-	.send()?;
+        .get("https://api.openai.com/v1/models")
+        .headers(headers)
+        .send()?;
     Ok(response)
 }
-
