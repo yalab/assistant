e7cd771a1bcfa756b9120f9bfe48f1f9c543ef51
yalab <rudeboyjet@gmail.com>
2023-04-21 18:51:05 +0900

    Can now handle data from a model retrieved via JSON.

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@ use dotenvy::dotenv;
 use reqwest::blocking::{Client, Response};
 use reqwest::header::{HeaderMap, HeaderValue};
 use serde::{Deserialize, Serialize};
-use serde_json;
 use std::env;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+pub struct Models {
+    object: String,
+    pub data: Vec<Model>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Model {
+    pub id: String,
 }
 
 pub fn api_key() -> String {
@@ -16,24 +20,22 @@ pub fn api_key() -> String {
     env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY environment variable is not found.")
 }
 
-pub fn call_chat_api(api_key: &str) -> Result<Response, Box<dyn std::error::Error>> {
+pub fn call_chat_api(api_key: &str, path: &str) -> Result<Response, Box<dyn std::error::Error>> {
     let mut headers = HeaderMap::new();
     let v = "Bearer ".to_owned() + api_key;
     headers.insert("Authorization", HeaderValue::from_str(&v).unwrap());
 
     let client = Client::new();
     let response = client
-        .get("https://api.openai.com/v1/models")
+        .get(format!("{}{}", "https://api.openai.com", path))
         .headers(headers)
         .send()?;
-
-    let point = Point { x: 1, y: 2 };
-
-    let serialized = serde_json::to_string(&point).unwrap();
-    println!("serialized = {}", serialized);
-
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized = {:?}", deserialized);
-
     Ok(response)
+}
+
+pub fn get_models() -> Models {
+    call_chat_api(&api_key(), "/v1/models")
+        .unwrap()
+        .json()
+        .unwrap()
 }
--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -8,7 +8,13 @@ fn api_key_test() {
 #[test]
 fn call_chat_api_test() {
     let api_key = api_key();
-    let result = call_chat_api(&api_key);
+    let result = call_chat_api(&api_key, "/v1/models");
     assert!(result.is_ok());
     assert_eq!("200 OK", format!("{}", result.unwrap().status()));
+}
+
+#[test]
+fn get_models_test() {
+    let models = get_models();
+    assert_eq!("babbage", models.data[0].id);
 }
